d51033a3d5e9d30a2ac422d72f755b446aa769a8
gobltpqls99 <gobltpqls99@naver.com>
2021-05-24 16:03:11 +0900

    ..

---
+++ test.js
@@ -0,0 +1,17 @@
+// const toggleBtn = document.querySelector('.toggleBtn');
+// const hello = document.querySelector('.hello');
+// const text = document.querySelector('.text');
+// const Seb = document.querySelector('.navi-wrap .navi-text');
+
+
+// toggleBtn.addEventListener('click', () => {
+//     hello.classList.toggle('active');
+//     text.classList.toggle('active');
+//     Seb.classList.toggle('active1');
+// });
+
+
+
+for(var i=1; i<6; i++){
+    console.log('*'.repeat(i));
+}
